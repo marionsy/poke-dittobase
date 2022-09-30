853d1d1466414b45608566c92d469aa3de076c40
Sharon-Lamanda <107377765+Sharon-Lamanda@users.noreply.github.com>
2022-09-29 19:26:39 -0700

    started on favorite routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,8 @@ const userRoutes = require('./userRoutes');
 const favoriteRoutes = require('./favoriteRoutes');
 const friendRoutes = require('./friendRoutes');
 
-router.use('/users', userRoutes);
+// router.use('/users', userRoutes);
 router.use('/favorites', favoriteRoutes);
-router.use('/friends', friendRoutes);
+// router.use('/friends', friendRoutes);
 
 module.exports = router;
